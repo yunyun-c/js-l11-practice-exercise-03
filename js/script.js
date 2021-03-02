2297e6227b74b8a2d7f0d564d9e396b486e1cbd8
Ann Cascarano <a.cascarano@gmail.com>
2021-03-02 15:42:11 -0500

    Solution for practice exercise 03

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const notRegistered = document.querySelector(".not-registered");
+
+const getRegistrationData = async function () {
+  const request = await fetch("../registrations.json");
+  const data = await request.json();
+  displayContactList(data);
+};
+
+const displayContactList = function (data) {
+  for (const student of data) {
+    if (student.registered === "no") {
+      const li = document.createElement("li");
+      li.innerText = student.name;
+      notRegistered.append(li);
+    }
+  }
+};
+
+getRegistrationData();
